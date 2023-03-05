bcd0158a750c063d1616b38f7f6eed1389eab90b
shenxl <shen.sxl@icloud.com>
2023-03-06 00:28:31 +0800

    为路由添加日志

--- src/routes/chat.py
+++ src/routes/chat.py
@@ -2,7 +2,9 @@
 from flask import request
 from flask_restful import Resource
 from src.replybot import ReplyBot, BotStatus
+from src.logs.logger import Logger
 
+logger = Logger(__name__)
 class Chat(Resource):
     def get(self):
         return {"result": "ok"}
@@ -30,4 +32,5 @@ class Chat(Resource):
         elif reply == BotStatus.REPLY:
             message = replybot.replyMsg                       
         # 返回响应数据
+        logger.info(message)
         return {"message": message}
