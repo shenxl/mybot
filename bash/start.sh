523092be3d63495ab4e12625f58fffe2a72801b4
shenxl <shen.sxl@icloud.com>
2023-03-07 21:04:50 +0800

    fix path for start.sh

--- bash/start.sh
+++ bash/start.sh
@@ -1,2 +1,2 @@
-nohup gunicorn -b 0.0.0.0:80 app:app --timeout 60  >/logs/mybot.log 2>&1 &
+nohup gunicorn -b 0.0.0.0:8888 ../app:app --timeout 60  >/logs/mybot.log 2>&1 &
 echo $! > /var/run/mybot.pid
